6ac67213a1e45ca8bc6219b09770d33fc12cf5de
Brandon Rilling <brandonrilling@Brandons-MacBook-Pro.local>
2019-06-13 09:27:37 -0400

    first commit

---
+++ if-then-practice/code.rb
@@ -0,0 +1,43 @@
+# puts "do you want 1 or 2 chocolate bars?"
+# input = gets.chomp
+# if input == 1
+#   puts "heres one chocolate bar"
+# else puts "heres 2 chocolate bars"
+# end
+
+# puts "would you like grapefruit or brussel sprouts?"
+# input = gets.chomp
+# if input == "brussel sprouts" || input == "grapefruit"
+#   puts "you entered my least favorite foods"
+# else puts "you were supposed to enter grapefruit or brussel sprouts"
+# end
+
+# puts "what hour (1-12) will you arive at the meeting"
+# input = gets.chomp.to_i
+# if input = >7 && <10
+#   puts "great you will be on time for the meeting"
+# else puts "you will not be ontime for the meeting"
+# end
+
+# input = "hello"
+# while input != "shut up"
+#   puts "#{input}"
+#   input = gets.chomp
+# end
+
+
+# name = "whats your name?"
+# while name != "stop"
+#   puts "#{name}"
+#   name = gets.chomp
+# end
+
+
+total = 0
+input = ""
+while total != 0
+input == gets.chomp.to_i
+  input += total
+end
+
+puts "#{total}"
